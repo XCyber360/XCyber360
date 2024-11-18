c0511ad7ad0f8da1e7dff062d2a9bf9e67bf5d7c
fortishield <161459699+FortiShield@users.noreply.github.com>
2024-11-18 23:13:29 +0000

    modified:   wodles/aws/__init__.py 	modified:   wodles/aws/buckets_s3/__init__.py 	modified:   wodles/aws/services/__init__.py 	modified:   wodles/aws/subscribers/__init__.py 	modified:   wodles/aws/tests/__init__.py 	modified:   wodles/docker-listener/tests/test_docker_listener.py 	modified:   wodles/gcloud/tests/test_tools.py

--- wodles/aws/buckets_s3/__init__.py
+++ wodles/aws/buckets_s3/__init__.py
@@ -1,6 +1,4 @@
-# Copyright (C) 2015, Xcyber360 Inc.
-# Created by Xcyber360, Inc. <info@xcyber360.com>.
-# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+
 
 from buckets_s3 import aws_bucket
 from buckets_s3 import cloudtrail
--- wodles/aws/services/__init__.py
+++ wodles/aws/services/__init__.py
@@ -1,6 +1,4 @@
-# Copyright (C) 2015, Xcyber360 Inc.
-# Created by Xcyber360, Inc. <info@xcyber360.com>.
-# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+
 
 from services import aws_service
 from services import cloudwatchlogs
